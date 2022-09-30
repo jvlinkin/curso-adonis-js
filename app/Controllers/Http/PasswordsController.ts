d4b3f22d834309b6b876c3d1c7c7d16c6f334fe8
JoaoPaiva <jvlimapaiva@gmail.com>
2022-09-30 16:21:12 -0300

    feat: tests about creating token to reset password

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -1,12 +1,19 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import crypto from 'crypto'
 
 export default class PasswordsController {
   public async forgotPassword({ request, response }: HttpContextContract) {
     const { email, resetPasswordUrl } = request.body()
+
     const user = await User.findByOrFail('email', email)
 
+    const token = crypto.randomBytes(20).toString('hex')
+
+    await user.related('tokens').updateOrCreate({ userId: user.id }, { token })
+
+    const resetPasswordUrlWithToken = `${resetPasswordUrl}?token=${token}`
     await Mail.send((message) => {
       message
         .from('no-reply@api.com')
@@ -15,7 +22,7 @@ export default class PasswordsController {
         .htmlView('email/forgotpassword', {
           productName: 'API',
           name: user.username,
-          resetPasswordUrl,
+          resetPasswordUrl: resetPasswordUrlWithToken,
         })
     })
 
--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -7,16 +7,30 @@ import Mail from '@ioc:Adonis/Addons/Mail'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Password', (group) => {
-  test
-    .only('it should send an email with the forgot password instructions', async (assert) => {
-      const user = await UserFactory.create()
+  test('it should send an email with the forgot password instructions', async (assert) => {
+    const user = await UserFactory.create()
 
-      Mail.trap((message) => {
-        assert.deepEqual(message.from, { address: 'no-reply@api.com' })
-        assert.deepEqual(message.to, [{ address: user.email }])
-        assert.equal(message.subject, 'API: Recuperação de senha.')
-        assert.include(message.html!, user.username)
+    Mail.trap((message) => {
+      assert.deepEqual(message.from, { address: 'no-reply@api.com' })
+      assert.deepEqual(message.to, [{ address: user.email }])
+      assert.equal(message.subject, 'API: Recuperação de senha.')
+      assert.include(message.html!, user.username)
+    })
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
       })
+      .expect(204)
+
+    Mail.restore()
+  }).timeout(0)
+
+  test
+    .only('it should create a reset token', async (assert) => {
+      const user = await UserFactory.create()
 
       await supertest(BASE_URL)
         .post('/forgot-password')
@@ -26,7 +40,9 @@ test.group('Password', (group) => {
         })
         .expect(204)
 
-      Mail.restore()
+      const tokens = await user.related('tokens').query()
+
+      assert.isNotEmpty(tokens)
     })
     .timeout(0)
 
