5489fa650531dab5a586ee47006dce19603ee9e7
JoaoPaiva <jvlimapaiva@gmail.com>
2022-10-06 16:54:15 -0300

    feat: creating the api-token when user login

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -3,8 +3,8 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class SessionsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const { email, password } = request.all()
-    await auth.attempt(email, password)
+    const token = await auth.use('api').attempt(email, password, { expiresIn: '2hours' })
 
-    return response.created({ user: auth.user })
+    return response.created({ user: auth.user, token })
   }
 }
--- test/users/sessions.spec.ts
+++ test/users/sessions.spec.ts
@@ -6,7 +6,7 @@ import { UserFactory } from 'Database/factories'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Password', (group) => {
-  test.only('it should authenticate an user', async (assert) => {
+  test('it should authenticate an user', async (assert) => {
     const plainPassword = 'test'
     const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
     const { body } = await supertest(BASE_URL)
@@ -15,6 +15,18 @@ test.group('Password', (group) => {
       .expect(201)
 
     assert.isDefined(body.user, 'User undefined.')
+    assert.equal(body.user.id, id)
+  })
+
+  test('it should return an api token when session is created.', async (assert) => {
+    const plainPassword = 'test'
+    const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    assert.isDefined(body.token, 'Token undefined.')
     assert.equal(body.user.id, id)
   })
 
